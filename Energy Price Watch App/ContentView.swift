5369cd9d824ed85c1f07a14465fdc043b11ce4fa
Robin Beer <contact@robin-beer.com>
2023-06-27 08:16:03 +0200

    Initial Commit

---
+++ Energy Price Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Energy Price Watch App
+//
+//  Created by Robin Beer on 27.06.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Energy Price Watch App/Energy_PriceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Energy_PriceApp.swift
+//  Energy Price Watch App
+//
+//  Created by Robin Beer on 27.06.23.
+//
+
+import SwiftUI
+
+@main
+struct Energy_Price_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
